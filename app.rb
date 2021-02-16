9988da21507c666e38b526332dc261e45570fe65
Adam Jonas <adamjonas25@gmail.com>
2021-02-16 12:37:17 +0000

    add redirect from post to get request

--- app.rb
+++ app.rb
@@ -17,6 +17,7 @@ class Battle < Sinatra::Base
   post '/play' do
     session[:player_1_name] = params[:player_1_name]
     session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
   end
 
   get '/play' do
