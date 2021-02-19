6b1b6bd6890d96097cd5b41d9c37a7f19b3e1a2e
Adam Jonas <adamjonas25@gmail.com>
2021-02-19 08:45:48 +0000

    reworks with game variable instead of players

--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -1,10 +1,24 @@
 describe Game do
-  let(:player) { instance_double(Player, take_damage: nil) }
+  let(:player_1) { instance_double(Player, take_damage: nil) }
+  let(:player_2) { instance_double(Player, take_damage: nil) }
+  subject        { described_class.new(player_1, player_2) }
 
   describe '#attack' do
     it 'reduces hp' do
-      expect(player).to receive :take_damage
-      subject.attack(player) 
+      expect(player_1).to receive :take_damage
+      subject.attack(player_1)
+    end
+  end
+
+  describe '#player_1' do
+    it 'returns player 1' do
+      expect(subject.player_1).to be player_1
+    end
+  end
+
+  describe '#player_2' do
+    it 'returns player 2' do
+      expect(subject.player_2).to be player_2
     end
   end
 end
